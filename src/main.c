e73674d246547721d0fb0fe0164cd21d2394154b
John Shmo <scottdig3@gmail.com>
2023-09-15 21:51:34 -0400

    Included SDL2

--- src/main.c
+++ src/main.c
@@ -1,8 +1,23 @@
 #include "shmo/user_input.h"
 #include <stdio.h>
 
-int main() {
+#include <SDL.h>
 
+int main(int argc, char *argv[]) {
+    SDL_Init(SDL_INIT_EVERYTHING);
 
+    SDL_Window *window = SDL_CreateWindow("My Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1080, 720, SDL_WINDOW_OPENGL);
+
+    while(true) {
+        SDL_Event event;
+        SDL_PollEvent(&event);
+        if (event.type == SDL_QUIT) {
+            break;
+        }
+    }
+
+    SDL_DestroyWindow(window);
+
+    SDL_Quit();
     return 0;
 }
