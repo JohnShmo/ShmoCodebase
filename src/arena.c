079342222dde9f7aa08a84e95315cb934800a7d1
John Shmo <scottdig3@gmail.com>
2023-09-28 16:35:32 -0400

    arena optimized

--- src/arena.c
+++ src/arena.c
@@ -16,6 +16,7 @@ struct LinearArenaPage {
     u8 *start;
     u8 *end;
     u8 *current;
+    u8 *previous_alloc;
     LinearArenaPage *next;
 };
 
@@ -26,15 +27,18 @@ local_fn bool arena_can_page_fit(LinearArenaPage *p, usize n) {
     return n <= diff;
 }
 
-local_fn void arena_page_create(LinearArenaPage *p, usize size) {
+local_fn bool arena_page_create(LinearArenaPage *p, usize size) {
     assert(p);
     assert(size);
     size = max(ARENA_MIN_PAGE_SIZE, size);
     p->start = malloc(size);
-    assert(p->start);
+    if (!p->start)
+        return false;
     p->current = p->start;
     p->end = p->start + size;
     p->next = nullptr;
+    p->previous_alloc = nullptr;
+    return true;
 }
 
 local_fn u8 *arena_page_alloc(LinearArenaPage *p, usize size) {
@@ -42,8 +46,17 @@ local_fn u8 *arena_page_alloc(LinearArenaPage *p, usize size) {
     assert(size);
     assert(arena_can_page_fit(p, size));
     u8 *result = p->current;
+    p->previous_alloc = result;
     p->current += size;
     return result;
+}
+
+local_fn u8 *arena_page_realloc(LinearArenaPage *p, usize size) {
+    assert(p);
+    assert(size);
+    assert(arena_can_page_fit(p, size));
+    p->current += size;
+    return p->previous_alloc;
 }
 
 local_fn void arena_page_destroy(LinearArenaPage *p) {
@@ -63,7 +76,8 @@ Arena *arena_create(void) {
 }
 
 void arena_destroy(Arena *a) {
-    assert(a);
+    if (!a)
+        return;
     arena_release(a);
     free(a);
 }
@@ -84,14 +98,24 @@ void *arena_malloc(Arena *a, usize n) {
     n = (usize)round_up_u64((u64)n, (u64)sizeof(usize));
 
     if (!a->pages) {
-        a->pages = malloc(sizeof(LinearArenaPage));
-        assert(a->pages);
-        arena_page_create(a->pages, n);
+        LinearArenaPage *page = malloc(sizeof(LinearArenaPage));
+        if (!page)
+            return nullptr;
+        if (!arena_page_create(page, n)){
+            free(page);
+            return nullptr;
+        }
+        a->pages = page;
     } else if (!arena_can_page_fit(a->pages, n)) {
         LinearArenaPage *old = a->pages;
-        a->pages = malloc(sizeof(LinearArenaPage));
-        assert(a->pages);
-        arena_page_create(a->pages, n);
+        LinearArenaPage *page = malloc(sizeof(LinearArenaPage));
+        if (!page)
+            return nullptr;
+        if (!arena_page_create(page, n)){
+            free(page);
+            return nullptr;
+        }
+        a->pages = page;
         a->pages->next = old;
     }
 
@@ -105,7 +129,8 @@ void *arena_calloc(Arena *a, usize n, usize size) {
     assert(size);
     n = n * size;
     void *result = arena_malloc(a, n);
-    assert(result);
+    if (!result)
+        return nullptr;
     memory_zero(result, n);
     return result;
 }
@@ -115,14 +140,17 @@ void *arena_realloc(Arena *a, void *p, usize n) {
     assert(n);
 
     if (!p) {
-        void *result = arena_malloc(a, n);
-        assert(result);
-        return result;
+        return arena_malloc(a, n);
+    }
+
+    if (a->pages && p == a->pages->previous_alloc && arena_can_page_fit(a->pages, n)) {
+        return arena_page_realloc(a->pages, n);
     }
 
     void *new_loc = arena_malloc(a, n);
-    assert(new_loc);
-    memory_copy(new_loc, p, n);
+    if (!new_loc)
+        return nullptr;
+    memcpy(new_loc, p, n);
 
     arena_free(a, p);
     return new_loc;
