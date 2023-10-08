5f8eec270a041040cad35e4692a2a63f4020a6f6
John Shmo <scottdig3@gmail.com>
2023-10-07 23:41:39 -0400

    Meep

--- src/sandbox/main.c
+++ src/sandbox/main.c
@@ -1,49 +1,21 @@
-#include "shmo/strings.h"
+#include "shmo/queue.h"
 
 int main(void) {
-    // Create a StringBuilder instance
-    StringBuilder *builder = string_builder_create(NULL); // Passing NULL to use the default allocator
-    if (!builder) {
-        fprintf(stderr, "Failed to create StringBuilder\n");
-        return 1;
+    Queue *q = queue_create(stdalloc);
+
+    if (!queue_enqueue(q, bytes_of_str("first"))) {
+        // handle error
+    }
+    queue_enqueue(q, bytes_of_str("second"));
+    queue_enqueue(q, bytes_of_str("third"));
+    queue_enqueue(q, bytes_of_str("fourth"));
+
+    while (!queue_empty(q)) {
+        const char *elm = bytes_to_str(queue_front(q));
+        printf("%s\n", elm);
+        queue_dequeue(q);
     }
 
-    // Push a character to StringBuilder
-    if (!string_builder_push_char(builder, 'A')) {
-        fprintf(stderr, "Failed to push character\n");
-    }
-
-    // Push a string to StringBuilder
-    if (!string_builder_push_str(builder, "BCD")) {
-        fprintf(stderr, "Failed to push string\n");
-    }
-
-    // Push a formatted string to StringBuilder
-    if (!string_builder_push_fmt(builder, " %d %s", 123, "EFG")) {
-        fprintf(stderr, "Failed to push formatted string\n");
-    }
-
-    // Print the constructed string
-    printf("Constructed String: %s\n", string_builder_cstr(builder));
-
-    // Insert a string at a specific position
-    if (!string_builder_insert_str(builder, 4, " Inserted")) {
-        fprintf(stderr, "Failed to insert string\n");
-    }
-
-    // Print the modified string
-    printf("Modified String: %s\n", string_builder_cstr(builder));
-
-    // Get and print a substring view from the StringBuilder
-    Strview view = string_builder_get_view(builder, r1u(5, 13));
-    if (strview_is_null(view)) {
-        fprintf(stderr, "Failed to get string view\n");
-    } else {
-        printf("Substring View: %.*s\n", (int)view.length, view.data);
-    }
-
-    // Destroy the StringBuilder instance
-    string_builder_destroy(builder);
-
+    queue_destroy(q);
     return 0;
 }
